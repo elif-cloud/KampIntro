8a0d6bc543b64f1305888a92f0e74342d97b58b7
elif-cloud <74643711+elif-cloud@users.noreply.github.com>
2021-02-25 23:43:01 +0300

    4. ders kodları Engin Demiroğ

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new GenericsIntro.MyList<string>();
+            isimler.Add("Elif");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
